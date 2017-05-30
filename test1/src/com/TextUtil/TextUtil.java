1703d81ec3f42737105d09fd84b3e8bf80b6d978
Ten000hours <502250806@qq.com>
2017-05-30 11:47:27 +0800

    还剩一个方法 textutil

--- test1/src/com/TextUtil/TextUtil.java
+++ test1/src/com/TextUtil/TextUtil.java
@@ -2,6 +2,7 @@ package com.TextUtil;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
@@ -23,19 +24,22 @@ public class TextUtil {
 			} else {
 				return false;
 			}
-		}else{
+		} else {
 			return false;
 		}
-		
 
 	}
 
-	public static boolean containbAbIsNull(TreeSet<Character> nvset,
-			String itemCharStr, Character c) {
+	/*
+	 * 形如bAb，b=空 param=
+	 */
+	public static boolean containbA_bIsNull(TreeSet<Character> nvset,
+			String itemCharStr, Character c,
+			HashMap<Character, ArrayList<Character>> experssionMap) {
 
-		String aStr=c.toString();
+		String aStr = c.toString();
 		
-		
+
 	}
 
 	public static boolean containsbA(TreeSet<Character> ntset,
@@ -54,11 +58,11 @@ public class TextUtil {
 			String itemCharStr, Character c,
 			HashMap<Character, ArrayList<Character>> expressionMap) {
 
-		String aStr=c.toString();
-		
+		String aStr = c.toString();
+
 		if (itemCharStr.contains(aStr)) {
 			String findSTR = null;
-			int aIndex=itemCharStr.indexOf(aStr);
+			int aIndex = itemCharStr.indexOf(aStr);
 			try {
 				findSTR = itemCharStr.substring(aIndex + 1, aIndex + 2);
 			} catch (Exception e) {
@@ -66,31 +70,88 @@ public class TextUtil {
 			}
 			if (ntset.contains(findSTR.charAt(0))) {
 				return true;
-			}else{
+			} else {
 				return false;
 			}
-		}else{
+		} else {
 			return false;
 		}
 	}
 
 	public static Character getA_lastChar(String itemCharStr, Character c) {
+		String atr = c.toString();
+		if (itemCharStr.contains(atr)) {
+			int index = itemCharStr.indexOf(atr);
+			String sub = itemCharStr.substring(index + 1, index + 2);
+			return sub.charAt(0);
+		}
+		return null;
+
 	}
 
+	/*
+	 * 是否是以空符号开始的
+	 */
 	public static boolean isEmptyStart(String selectExp) {
+
+		if (selectExp.charAt(0) == 'ε') {
+			return true;
+		}
+		return false;
 	}
 
+	/*
+	 * 是否是以终结符开始
+	 */
 	public static boolean isNtStart(TreeSet<Character> ntset, String selectExp) {
+		char a = selectExp.charAt(0);
+		if (ntset.contains(a)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
+	/**
+	 * @param nvset 非终结符集合
+	 * @param selectMap 选中的string串
+	 * 是否是以非终结符开始的
+	 */
 	public static boolean isNvStart(TreeSet<Character> nvset, String selectExp) {
+		char a = selectExp.charAt(0);
+		if (nvset.contains(a)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
+	 /** 
+     * 查找产生式 
+     *  
+     * @param selectMap 
+     * @param peek 
+     *            当前Nv 
+     * @param charAt 
+     *            当前字符 
+     * @return 
+     */ 
 	public static String findUseExp(
 			TreeMap<Character, HashMap<String, TreeSet<Character>>> selectMap,
 			Character peek, char charat) {
-
+		HashMap<String, TreeSet<Character>> Hmap=selectMap.get(peek);
+		try {
+			Set<String> Exp=Hmap.keySet();
+			for (String useExp : Exp) {
+				TreeSet<Character> ts=Hmap.get(useExp);
+				if (ts.contains(charat)) {
+					return useExp;
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
-	public static boolean getAlastChar(String itemCharStr,Character a){}
-	
 }
